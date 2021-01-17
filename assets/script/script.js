eb4ac630f504b974222f656b9c296025652a1189
Nazar Archakov <nazar.archakov222@gmail.com>
2021-01-17 19:21:30 +0300

    made some changes

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,5 +1,4 @@
 // PopUp Windows for Login And Registration
-
 let loginBtn = document.querySelector('.nav__login');
 let regBtn = document.querySelector('.nav__reg');
 let loginPopUp = document.querySelector('.log__popup');
@@ -8,35 +7,35 @@ let outsidePopUp = document.querySelectorAll('.popup')
 let sidebarLogin = document.querySelector('.burger__log')
 let sidebarReg = document.querySelector('.burger__reg')
 
-loginBtn.addEventListener('click', () => {
-  loginPopUp.style.display = 'block'
-})
-
-sidebarLogin.addEventListener('click', (e) => {
-  e.preventDefault()
-  loginPopUp.style.display = 'block'
-})
-
-sidebarReg.addEventListener('click', (e) => {
-  e.preventDefault()
-  regPopUp.style.display = 'block'
-})
-
-regBtn.addEventListener('click', () => {
-  regPopUp.style.display = 'block'
-})
-
-outsidePopUp.forEach((modal) => {
-  modal.addEventListener('click', (e) => {
-    if (e.target == modal) {
-      modal.style.display = 'none';
-    }
+ if (loginBtn) {
+  loginBtn.addEventListener('click', () => {
+    loginPopUp.style.display = 'block'
   })
-})
+  
+  sidebarLogin.addEventListener('click', (e) => {
+    e.preventDefault()
+    loginPopUp.style.display = 'block'
+  })
+  
+  sidebarReg.addEventListener('click', (e) => {
+    e.preventDefault()
+    regPopUp.style.display = 'block'
+  })
+  
+  regBtn.addEventListener('click', () => {
+    regPopUp.style.display = 'block'
+  })
+  
+  outsidePopUp.forEach((modal) => {
+    modal.addEventListener('click', (e) => {
+      if (e.target == modal) {
+        modal.style.display = 'none';
+      }
+    })
+  })
+ }
 
 // Hero Burger
-
-
 let burgerBtn = document.querySelector('.burger-icon');
 let sidebar = document.querySelector('.vertical__bar');
 
@@ -77,21 +76,10 @@ closeBtn.addEventListener('click', () => {
 
 // Accordion Questions
 
-// let btns = document.querySelectorAll('.toggle-btn');
-
-// btns.forEach((btn) => {
-//   btn.addEventListener('click',(e) => {
-//     let question = e.currentTarget.parentElement.parentElement
-//     question.classList.toggle('show-text')
-//   })
-// })
-
 let questions = document.querySelectorAll('.accordion__item');
 
 questions.forEach((question) => {
-  // console.log(question)
   let btn = question.querySelector('.toggle-btn');
-  // console.log(btn)
   btn.addEventListener('click', () => {
 
 
