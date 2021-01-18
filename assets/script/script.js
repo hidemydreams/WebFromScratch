f7acdfacc04636d15f00bbded24c6f32290d82cc
Nazar Archakov <nazar.archakov222@gmail.com>
2021-01-18 03:28:52 +0300

    fix: fix problems and bugs

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,4 +1,5 @@
 // PopUp Windows for Login And Registration
+let body = document.querySelector('body')
 let loginBtn = document.querySelector('.nav__login');
 let regBtn = document.querySelector('.nav__reg');
 let loginPopUp = document.querySelector('.log__popup');
@@ -9,7 +10,8 @@ let sidebarReg = document.querySelector('.burger__reg');
 
  if (loginBtn) {
   loginBtn.addEventListener('click', () => {
-    loginPopUp.style.display = 'block'
+    loginPopUp.style.display = 'block';
+    body.style.overflow = 'hidden';
   })
   
   sidebarLogin.addEventListener('click', (e) => {
@@ -24,12 +26,14 @@ let sidebarReg = document.querySelector('.burger__reg');
   
   regBtn.addEventListener('click', () => {
     regPopUp.style.display = 'block'
+    body.style.overflow = 'hidden';
   })
   
   outsidePopUp.forEach((modal) => {
     modal.addEventListener('click', (e) => {
       if (e.target == modal) {
         modal.style.display = 'none';
+        body.style.overflow = 'auto';
       }
     })
   })
@@ -38,25 +42,29 @@ let sidebarReg = document.querySelector('.burger__reg');
 // Hero Burger
 let burgerBtn = document.querySelector('.burger-icon');
 let sidebar = document.querySelector('.vertical__bar');
+let closeBurger = document.querySelector('.close-burger');
 
 burgerBtn.addEventListener('click', () => {
-    sidebar.classList.toggle('burger');
+    sidebar.classList.add('burger');
+})
+
+closeBurger.addEventListener('click', () => {
+    sidebar.classList.remove('burger');
 })
 
 // Search Button
 
 let searchBtn = document.querySelector('.search-btn');
 let closeSearch = document.querySelector('.search-close');
+let searchForm = document.querySelector('.search-form')
 
 searchBtn.addEventListener('click', () => {
-    sidebar.classList.toggle('open-search');
+    searchForm.classList.add('open-search');
 })
 
 closeSearch.addEventListener('click', () => {
-    sidebar.classList.toggle('open-search');
+    searchForm.classList.remove('open-search');
 })
-
-
 
 // PopUp for Contact Page
 
@@ -68,32 +76,30 @@ if (formBtn) {
     formBtn.addEventListener('click', (e) => {
         e.preventDefault();
         contactPop.style.display = 'block';
+        body.style.overflow = 'hidden';
+
     })
     closeBtn.addEventListener('click', () => {
         contactPop.style.display = 'none';
+        body.style.overflow = 'auto';
     })
 }
 
 // Accordion Questions
 
 let questions = document.querySelectorAll('.accordion__item');
-
 questions.forEach((question) => {
-  let btn = question.querySelector('.toggle-btn');
-  btn.addEventListener('click', () => {
-
-
-        questions.forEach((item) => {
-            if (item !== question) {
-                item.classList.remove('show-text')
-            }
-        })
-
-
-
+    question.addEventListener('click', () => {
         question.classList.toggle('show-text')
-    })
-})
+
+  
+          questions.forEach((item) => {
+              if (item !== question) {
+                  item.classList.remove('show-text')
+              }
+          })
+      })
+  })
 
 // Tabs For FullPage Registration
 
@@ -116,58 +122,57 @@ if (signUpContent) {
         loginContent.style.display = 'flex';
         signUpContent.style.display = 'none';
     })
-
 }
-//PopUp for rating
-let starBtns = document.querySelectorAll('.star');
-let modalComment = document.querySelector('.rating_popup');
+// //PopUp for rating
+// let starBtns = document.querySelectorAll('.star');
+// let modalComment = document.querySelector('.rating_popup');
 
-starBtns.forEach((starBtn) => {
-    starBtn.addEventListener('click', () => {
-        modalComment.style.display = 'block';
-    })
-})
-let ratingBtn = document.querySelector('.btn_rating');
-    ratingBtn.addEventListener('click', () => {
-    modalComment.style.display = 'none';
-})
-
-
-
-//PopUp for comments
-let taskBtn = document.querySelector('.link_comments');
-let taskModal = document.querySelector('.comments_popup');
-if (taskBtn) {
-    taskBtn.addEventListener('click', (e) => {
-        e.preventDefault()
-        taskModal.style.display = 'block';
-    })
-}
-let quitBtn = document.querySelector('.btn_quit');
-quitBtn.addEventListener('click', () => {
-    taskModal.style.display = 'none';
-})
-
-//PopUp for tasks
-let tasksBtn = document.querySelector('.tasks');
-let tasksModal = document.querySelector('.tasks_popup');
-
-tasksBtn.addEventListener('click', (e) => {
-    e.preventDefault()
-    tasksModal.style.display = 'block';
-})
-let taskBtnQuit = document.querySelector('.btn_tasks');
-taskBtnQuit.addEventListener('click', () => {
-    tasksModal.style.display = 'none';
-})
+// starBtns.forEach((starBtn) => {
+//     starBtn.addEventListener('click', () => {
+//         modalComment.style.display = 'block';
+//     })
+// })
+// let ratingBtn = document.querySelector('.btn_rating');
+//     ratingBtn.addEventListener('click', () => {
+//     modalComment.style.display = 'none';
+// })
 
 
 
-//PopUp for mistake
-let mistakePopup = document.querySelector('.mistake');
-let mistakeModal = document.querySelector('.mistake_popup');
-let mistakeBtn = document.querySelector('.btn_article');
-mistakePopup.addEventListener('click', (e) => {
-    e.preventDefault()
-    mistakeModal.style.display = 'block';
-})
+// //PopUp for comments
+// let taskBtn = document.querySelector('.link_comments');
+// let taskModal = document.querySelector('.comments_popup');
+// if (taskBtn) {
+//     taskBtn.addEventListener('click', (e) => {
+//         e.preventDefault()
+//         taskModal.style.display = 'block';
+//     })
+// }
+// let quitBtn = document.querySelector('.btn_quit');
+// quitBtn.addEventListener('click', () => {
+//     taskModal.style.display = 'none';
+// })
+
+// //PopUp for tasks
+// let tasksBtn = document.querySelector('.tasks');
+// let tasksModal = document.querySelector('.tasks_popup');
+
+// tasksBtn.addEventListener('click', (e) => {
+//     e.preventDefault()
+//     tasksModal.style.display = 'block';
+// })
+// let taskBtnQuit = document.querySelector('.btn_tasks');
+// taskBtnQuit.addEventListener('click', () => {
+//     tasksModal.style.display = 'none';
+// })
+
+
+
+// //PopUp for mistake
+// let mistakePopup = document.querySelector('.mistake');
+// let mistakeModal = document.querySelector('.mistake_popup');
+// let mistakeBtn = document.querySelector('.btn_article');
+// mistakePopup.addEventListener('click', (e) => {
+//     e.preventDefault()
+//     mistakeModal.style.display = 'block';
+// })
